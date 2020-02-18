847bfc4bfec9419cc9f2083e2ea02e0b95ce438e
Kirill Shulga <K.Shulga@skillunion.ru>
2020-02-18 16:17:34 +0300

    adding new widgets and icons

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { IFeature } from '@dapplets/dapplet-extension'
 import { sayHello } from './helper';
+import CHECKED_ICON_SHIELD from './check_circle_shield.svg'
 import CHECKED_ICON from './check_circle.svg'
+import CHECKED_ICON_SHIELD_YELLOW from './check_circle_shield_yellow.svg'
 import CLOSE_ICON from './close_circle.svg'
 import STAR_ICON from './star_circle.svg'
 
@@ -13,9 +15,9 @@ export default class Feature implements IFeature {
 
     constructor() {
         const server = Core.connect<{ pm_num: string }>({ url: "wss://localhost:8080/feature-1" });
-        const { button, badge, profile } = this.adapter.widgets;
+        const { badge } = this.adapter.widgets;
         this.config = {
-            AVATAR_BADGE: [
+            TWEET_AVATAR_BADGE: [
                 badge({
                     initial: "DEFAULT",
                     "DEFAULT": {
@@ -33,20 +35,10 @@ export default class Feature implements IFeature {
                     },
                     "HIDDEN": {
                         hidden: false,
-                        img: CHECKED_ICON,
+                        img: CHECKED_ICON_SHIELD,
                         //img: "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjxzdmcgaWQ9IkxheWVyXzEiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDYxMiA3OTI7IiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCA2MTIgNzkyIiB4bWw6c3BhY2U9InByZXNlcnZlIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj48c3R5bGUgdHlwZT0idGV4dC9jc3MiPgoJLnN0MHtmaWxsOiM0MUFENDk7fQo8L3N0eWxlPjxnPjxwYXRoIGNsYXNzPSJzdDAiIGQ9Ik01NjIsMzk2YzAtMTQxLjQtMTE0LjYtMjU2LTI1Ni0yNTZTNTAsMjU0LjYsNTAsMzk2czExNC42LDI1NiwyNTYsMjU2UzU2Miw1MzcuNCw1NjIsMzk2TDU2MiwzOTZ6ICAgIE01MDEuNywyOTYuM2wtMjQxLDI0MWwwLDBsLTE3LjIsMTcuMkwxMTAuMyw0MjEuM2w1OC44LTU4LjhsNzQuNSw3NC41bDE5OS40LTE5OS40TDUwMS43LDI5Ni4zTDUwMS43LDI5Ni4zeiIvPjwvZz48L3N2Zz4=",
                         vertical: "top",
-                        horizontal: "left",
-                        init: (ctx, me) => {
-                            console.log('hidden', me, ctx);
-                            //if (ctx.authorUsername === "@Ethernian") {
-                            //    me.setState("HIDDEN");
-                            //}
-                        } ,
-                        exec: (ctx, me) => { // ToDo: rename exec() to onclick()
-                            console.log('hidden exec', me, ctx);
-                            me.setState("DISABLED");
-                        } 
+                        horizontal: "left" 
                     },
                     "DISABLED": {
                         hidden: false,
@@ -60,7 +52,7 @@ export default class Feature implements IFeature {
                     }
                 })
             ],
-            USERNAME_BADGE:  [
+            TWEET_USERNAME_BADGE:  [
                 badge({
                     initial: "DEFAULT",
                     "DEFAULT": {
@@ -73,7 +65,27 @@ export default class Feature implements IFeature {
                     },
                     "HIDDEN": {
                         hidden: false,
-                        img: CHECKED_ICON,
+                        img: CHECKED_ICON_SHIELD,
+                        //img: "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjxzdmcgaWQ9IkxheWVyXzEiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDYxMiA3OTI7IiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCA2MTIgNzkyIiB4bWw6c3BhY2U9InByZXNlcnZlIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj48c3R5bGUgdHlwZT0idGV4dC9jc3MiPgoJLnN0MHtmaWxsOiM0MUFENDk7fQo8L3N0eWxlPjxnPjxwYXRoIGNsYXNzPSJzdDAiIGQ9Ik01NjIsMzk2YzAtMTQxLjQtMTE0LjYtMjU2LTI1Ni0yNTZTNTAsMjU0LjYsNTAsMzk2czExNC42LDI1NiwyNTYsMjU2UzU2Miw1MzcuNCw1NjIsMzk2TDU2MiwzOTZ6ICAgIE01MDEuNywyOTYuM2wtMjQxLDI0MWwwLDBsLTE3LjIsMTcuMkwxMTAuMyw0MjEuM2w1OC44LTU4LjhsNzQuNSw3NC41bDE5OS40LTE5OS40TDUwMS43LDI5Ni4zTDUwMS43LDI5Ni4zeiIvPjwvZz48L3N2Zz4=",
+                        vertical: "top",
+                        horizontal: "right"
+                    }
+                })
+            ],
+            HEADING_USERNAME_BADGE:  [
+                badge({
+                    initial: "DEFAULT",
+                    "DEFAULT": {
+                        hidden: true,
+                        init: (ctx, me) => {
+                            //if (ctx.authorUsername === "@Ethernian") {
+                                me.setState("HIDDEN");
+                            //}
+                        }        
+                    },
+                    "HIDDEN": {
+                        hidden: false,
+                        img: CHECKED_ICON_SHIELD,
                         //img: "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjxzdmcgaWQ9IkxheWVyXzEiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDYxMiA3OTI7IiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCA2MTIgNzkyIiB4bWw6c3BhY2U9InByZXNlcnZlIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj48c3R5bGUgdHlwZT0idGV4dC9jc3MiPgoJLnN0MHtmaWxsOiM0MUFENDk7fQo8L3N0eWxlPjxnPjxwYXRoIGNsYXNzPSJzdDAiIGQ9Ik01NjIsMzk2YzAtMTQxLjQtMTE0LjYtMjU2LTI1Ni0yNTZTNTAsMjU0LjYsNTAsMzk2czExNC42LDI1NiwyNTYsMjU2UzU2Miw1MzcuNCw1NjIsMzk2TDU2MiwzOTZ6ICAgIE01MDEuNywyOTYuM2wtMjQxLDI0MWwwLDBsLTE3LjIsMTcuMkwxMTAuMyw0MjEuM2w1OC44LTU4LjhsNzQuNSw3NC41bDE5OS40LTE5OS40TDUwMS43LDI5Ni4zTDUwMS43LDI5Ni4zeiIvPjwvZz48L3N2Zz4=",
                         vertical: "top",
                         horizontal: "left"
@@ -81,7 +93,7 @@ export default class Feature implements IFeature {
                 })
             ],
             PROFILE_USERNAME_BADGE:  [
-                profile({
+                badge({
                     initial: "DEFAULT",
                     "DEFAULT": {
                         hidden: true,
@@ -93,7 +105,7 @@ export default class Feature implements IFeature {
                     },
                     "HIDDEN": {
                         hidden: false,
-                        img: STAR_ICON,
+                        img: CHECKED_ICON_SHIELD_YELLOW,
                         //img: "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjxzdmcgaWQ9IkxheWVyXzEiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDYxMiA3OTI7IiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCA2MTIgNzkyIiB4bWw6c3BhY2U9InByZXNlcnZlIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj48c3R5bGUgdHlwZT0idGV4dC9jc3MiPgoJLnN0MHtmaWxsOiM0MUFENDk7fQo8L3N0eWxlPjxnPjxwYXRoIGNsYXNzPSJzdDAiIGQ9Ik01NjIsMzk2YzAtMTQxLjQtMTE0LjYtMjU2LTI1Ni0yNTZTNTAsMjU0LjYsNTAsMzk2czExNC42LDI1NiwyNTYsMjU2UzU2Miw1MzcuNCw1NjIsMzk2TDU2MiwzOTZ6ICAgIE01MDEuNywyOTYuM2wtMjQxLDI0MWwwLDBsLTE3LjIsMTcuMkwxMTAuMyw0MjEuM2w1OC44LTU4LjhsNzQuNSw3NC41bDE5OS40LTE5OS40TDUwMS43LDI5Ni4zTDUwMS43LDI5Ni4zeiIvPjwvZz48L3N2Zz4=",
                         vertical: "top",
                         horizontal: "left"
@@ -101,7 +113,7 @@ export default class Feature implements IFeature {
                 })
             ],
             PROFILE_AVATAR_BADGE:  [
-                profile({
+                badge({
                     initial: "DEFAULT",
                     "DEFAULT": {
                         hidden: true,
@@ -113,7 +125,27 @@ export default class Feature implements IFeature {
                     },
                     "HIDDEN": {
                         hidden: false,
-                        img: STAR_ICON,
+                        img: CHECKED_ICON_SHIELD_YELLOW,
+                        //img: "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjxzdmcgaWQ9IkxheWVyXzEiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDYxMiA3OTI7IiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCA2MTIgNzkyIiB4bWw6c3BhY2U9InByZXNlcnZlIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj48c3R5bGUgdHlwZT0idGV4dC9jc3MiPgoJLnN0MHtmaWxsOiM0MUFENDk7fQo8L3N0eWxlPjxnPjxwYXRoIGNsYXNzPSJzdDAiIGQ9Ik01NjIsMzk2YzAtMTQxLjQtMTE0LjYtMjU2LTI1Ni0yNTZTNTAsMjU0LjYsNTAsMzk2czExNC42LDI1NiwyNTYsMjU2UzU2Miw1MzcuNCw1NjIsMzk2TDU2MiwzOTZ6ICAgIE01MDEuNywyOTYuM2wtMjQxLDI0MWwwLDBsLTE3LjIsMTcuMkwxMTAuMyw0MjEuM2w1OC44LTU4LjhsNzQuNSw3NC41bDE5OS40LTE5OS40TDUwMS43LDI5Ni4zTDUwMS43LDI5Ni4zeiIvPjwvZz48L3N2Zz4=",
+                        vertical: "top",
+                        horizontal: "left"
+                    }
+                })
+            ],
+            SUSPENDED_USERNAME_BADGE:  [
+                badge({
+                    initial: "DEFAULT",
+                    "DEFAULT": {
+                        hidden: true,
+                        init: (ctx, me) => {
+                            //if (ctx.authorUsername === "@Ethernian") {
+                                me.setState("HIDDEN");
+                            //}
+                        }        
+                    },
+                    "HIDDEN": {
+                        hidden: false,
+                        img: CHECKED_ICON_SHIELD_YELLOW,
                         //img: "data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjxzdmcgaWQ9IkxheWVyXzEiIHN0eWxlPSJlbmFibGUtYmFja2dyb3VuZDpuZXcgMCAwIDYxMiA3OTI7IiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCA2MTIgNzkyIiB4bWw6c3BhY2U9InByZXNlcnZlIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj48c3R5bGUgdHlwZT0idGV4dC9jc3MiPgoJLnN0MHtmaWxsOiM0MUFENDk7fQo8L3N0eWxlPjxnPjxwYXRoIGNsYXNzPSJzdDAiIGQ9Ik01NjIsMzk2YzAtMTQxLjQtMTE0LjYtMjU2LTI1Ni0yNTZTNTAsMjU0LjYsNTAsMzk2czExNC42LDI1NiwyNTYsMjU2UzU2Miw1MzcuNCw1NjIsMzk2TDU2MiwzOTZ6ICAgIE01MDEuNywyOTYuM2wtMjQxLDI0MWwwLDBsLTE3LjIsMTcuMkwxMTAuMyw0MjEuM2w1OC44LTU4LjhsNzQuNSw3NC41bDE5OS40LTE5OS40TDUwMS43LDI5Ni4zTDUwMS43LDI5Ni4zeiIvPjwvZz48L3N2Zz4=",
                         vertical: "top",
                         horizontal: "left"
