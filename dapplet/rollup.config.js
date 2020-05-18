856b9f0d96b62b1f30df6d91c35a9fa1d71c6678
Alexander Sakhaev <alsakhaev@gmail.com>
2020-05-18 14:01:31 +0300

    feat: show green badge when account exists in registry

--- dapplet/rollup.config.js
+++ dapplet/rollup.config.js
@@ -2,6 +2,12 @@ import typescript from '@rollup/plugin-typescript';
 import image from '@rollup/plugin-image';
 import serve from 'rollup-plugin-serve';
 import fs from 'fs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+const showAddress = () => ({ 
+    load: () => console.log('\x1b[35m%s\x1b[0m', `Current registry address: https://localhost:10001/dapplets.json`) 
+});
 
 export default [{
     input: 'src/index.ts',
@@ -10,12 +16,19 @@ export default [{
         format: 'cjs',
         exports: 'named'
     }],
-    plugins: [typescript(), image(), serve({
-        https: {
-            key: fs.readFileSync('./secret/server.key'),
-            cert: fs.readFileSync('./secret/server.cert'),
-            //ca: fs.readFileSync('/path/to/ca.pem')
-        },
-        contentBase: ''
-    }), { load: () => console.log('\x1b[35m%s\x1b[0m', `Current registry address: https://localhost:10001/dapplets.json`) }]
+    plugins: [
+        typescript(), 
+        resolve({ browser: true }),
+        commonjs(),
+        image(), 
+        serve({
+            https: {
+                key: fs.readFileSync('./secret/server.key'),
+                cert: fs.readFileSync('./secret/server.cert'),
+                //ca: fs.readFileSync('/path/to/ca.pem')
+            },
+            contentBase: ''
+        }),
+        showAddress()
+    ]
 }];
--- dapplet/src/index.ts
+++ dapplet/src/index.ts
@@ -4,6 +4,15 @@ import ICON_DAPPLET from './icons/dapplet.png';
 import ICON_GREEN from './icons/green.svg';
 import ICON_YELLOW from './icons/yellow.svg';
 import ICON_RED from './icons/red.svg';
+import ICON_LOADING from './icons/loading.svg';
+
+import abi from './abi';
+import ethers from 'ethers';
+
+type Account = {
+    domainId: number; // 1 - twitter, 2 - ens
+    name: string;
+}
 
 @Injectable
 export default class Feature implements IFeature {
@@ -17,11 +26,20 @@ export default class Feature implements IFeature {
     private _currentProve: string = null;
     private _currentAddress: string = null;
 
+    private _contract: ethers.Contract;
+
+    private _accounts = new Map<string, Promise<Account[]>>();
+
     constructor() {
         const wallet = Core.wallet();
         const overlay = Core.overlay({ url: 'https://localhost:3000', title: 'Identity Management' });
         const { badge, button } = this.adapter.widgets;
         const { popup } = this.adapter2.widgets;
+
+        const provider = ethers.getDefaultProvider('rinkeby');
+        this._contract = new ethers.Contract('0x3ecb70ec3e63f2c0e11324ae9f1332aa062a85cc', abi, provider);
+
+        console.log('contract', this._contract);
 
         this.config = {
             PROFILE_BUTTON_GROUP: [
@@ -69,15 +87,18 @@ export default class Feature implements IFeature {
                 badge({
                     initial: "DEFAULT",
                     "DEFAULT": {
-                        hidden: false,
-                        img: ICON_GREEN,
+                        img: ICON_LOADING,
                         vertical: "bottom",
                         horizontal: "left",
-                        init: (ctx, me) => {
-                            //if (ctx.authorUsername === "@Ethernian") {
-                            //me.setState("HIDDEN");
-                            //}
+                        init: async (ctx, me) => {
+                            const accounts = await this._getAccounts({ domainId: 1, name: ctx.authorUsername.toLowerCase() });
+                            me.setState((accounts.length > 0) ? "GREEN" : "HIDDEN");
                         }
+                    },
+                    "GREEN": {
+                        img: ICON_GREEN,
+                        vertical: "bottom",
+                        horizontal: "left"
                     },
                     "HIDDEN": {
                         hidden: true
@@ -88,15 +109,18 @@ export default class Feature implements IFeature {
                 badge({
                     initial: "DEFAULT",
                     "DEFAULT": {
-                        hidden: false,
+                        img: ICON_LOADING,
+                        vertical: "bottom",
+                        horizontal: "left",
+                        init: async (ctx, me) => {
+                            const accounts = await this._getAccounts({ domainId: 1, name: ctx.authorUsername.toLowerCase() });
+                            me.setState((accounts.length > 0) ? "GREEN" : "HIDDEN");
+                        }
+                    },
+                    "GREEN": {
                         img: ICON_GREEN,
                         vertical: "top",
-                        horizontal: "left",
-                        init: (ctx, me) => {
-                            //if (ctx.authorUsername === "@Ethernian") {
-                            //me.setState("HIDDEN");
-                            //}
-                        }
+                        horizontal: "left"
                     },
                     "HIDDEN": {
                         hidden: true
@@ -104,6 +128,16 @@ export default class Feature implements IFeature {
                 })
             ]
         };
+    }
+
+    private async _getAccounts(account: Account): Promise<Account[]> {
+        const key = `${account.domainId}/${account.name}`;
+
+        if (!this._accounts.has(key)) {
+            this._accounts.set(key, this._contract.getAccounts(account));
+        }
+
+        return this._accounts.get(key);
     }
 
     public activate() {
