bb2a37bdeb7e216909e7967d81b04dfedfa3f602
Alexander Sakhaev <alsakhaev@gmail.com>
2020-05-20 16:44:51 +0300

    refactor: removed ssl from dapplet

--- dapplet/rollup.config.js
+++ dapplet/rollup.config.js
@@ -1,12 +1,11 @@
 import typescript from '@rollup/plugin-typescript';
 import image from '@rollup/plugin-image';
 import serve from 'rollup-plugin-serve';
-import fs from 'fs';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 const showAddress = () => ({ 
-    load: () => console.log('\x1b[35m%s\x1b[0m', `Current registry address: https://localhost:10001/dapplets.json`) 
+    load: () => console.log('\x1b[35m%s\x1b[0m', `Current registry address: http://localhost:10001/dapplets.json`) 
 });
 
 export default [{
@@ -21,14 +20,7 @@ export default [{
         resolve({ browser: true }),
         commonjs(),
         image(), 
-        serve({
-            https: {
-                key: fs.readFileSync('./secret/server.key'),
-                cert: fs.readFileSync('./secret/server.cert'),
-                //ca: fs.readFileSync('/path/to/ca.pem')
-            },
-            contentBase: ''
-        }),
+        serve(),
         showAddress()
     ]
 }];
