781087be6b1c5aae9ae3c4a1377849cb8c355e9a
Alexander Sakhaev <alsakhaev@gmail.com>
2021-01-19 17:11:41 +0300

    refactor: new swarm bee nodes support

--- overlay/src/services/textService.ts
+++ overlay/src/services/textService.ts
@@ -1,16 +1,17 @@
 export class TextService {
     async load(id: string): Promise<string> {
-        const response = await fetch("https://swarm-gateways.net/bzz-raw:/" + id);
+        const response = await fetch("https://gateway.ethswarm.org/files/" + id);
         return await response.text();
     }
 
     async save(text: string): Promise<string> {
-        const response = await fetch("https://swarm-gateways.net/bzz-raw:/", {
+        const response = await fetch("https://gateway.ethswarm.org/files", {
             method: 'POST',
             headers: { 'Content-Type': 'text/plain' },
             body: text
         });
     
-        return await response.text();
+        const result = await response.json();
+        return result.reference;
     }
 }
